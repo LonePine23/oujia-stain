788f7143c3075f7f2c445da92bc8adf4b2ca0bcd
LonePine23 <geordie.business@hotmail.com>
2026-09-07 10:16:47 +1000

    Add files via upload

--- yummers/sw.js
+++ yummers/sw.js
@@ -15,7 +15,7 @@
    old version even after uploading a new one.
    =========================================================================== */
 
-const CACHE_VERSION = 'yummers-v4';
+const CACHE_VERSION = 'yummers-v5';
 
 // The "app shell": the files that make up the app itself, as opposed to your
 // data. Paths are relative, so this works from any folder on your site.
